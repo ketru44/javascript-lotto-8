d002db0d90802182d9f4713aa5467db394a5726f
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2025-11-03 01:56:35 +0900

    feat(constants): 로또 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const LOTTO_CONSTANTS = Object.freeze({
+  TICKET_PRICE: 1000,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  NUMBERS_PER_TICKET: 6,
+});
+
+export const PRIZE_TABLE = Object.freeze({
+  1: 2_000_000_000,
+  2: 30_000_000,    // 5개 일치 + 보너스 번호 일치
+  3: 1_500_000,
+  4: 50_000,
+  5: 5_000,
+});
