21af86cb8ec46cd86eb54a6e66356cad8a3599cf
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2025-11-03 18:32:17 +0900

    feat(util): implement divison function
    구입 금액으로 부터 티켓의 수를 구하기 위한 나눗셈 함수 구현하였습니다.

--- __tests__/UtilUnit.test.js
+++ __tests__/UtilUnit.test.js
@@ -1,4 +1,4 @@
-import { toNumber, parseToArrayByComma } from "../src/utils/parsing";
+import { toNumber, parseToArrayByComma, devisionNumber} from "../src/utils/parsing";
 
 describe("파싱 유틸 검증", () => {
   test("숫자로 이루어진 문자열을 숫자 타입으로 변환한다.", () => {
@@ -6,5 +6,8 @@ describe("파싱 유틸 검증", () => {
   });
   test("쉼표를 포함하는 문자열을 쉼표로 나누어 숫자 배열로 변환한다.", () => {
     expect(parseToArrayByComma("1,2,3")).toStrictEqual([1, 2, 3]);
+  });
+  test("구입 금액을 단위(1000)으로 나누어 티켓 장수를 반환하다.", () => {
+    expect(devisionNumber(5000, 1000)).toBe(5);
   })
 })
--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -1,3 +1,5 @@
 export const toNumber = (str) => Number(str);
 
 export const parseToArrayByComma = (arr) => arr.split(",").map(n => toNumber(n.trim()));
+
+export const devisionNumber = (divdend, divisor) => divdend / divisor;
