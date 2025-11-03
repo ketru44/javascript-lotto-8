04163be5f2935da45a6685cfb65473626a515eb7
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2025-11-03 22:11:08 +0900

    feat(app): print purchased amount and generated numbers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { INPUT_QUESTION } from "./constants/ioMsg";
+import { LOTTO_CONSTANTS } from "./constants/lotto";
 import { askUntilValid } from "./askUntilValid";
-import { parseToArrayByComma, toNumber } from "./utils/parsing";
+import { devisionNumber, parseToArrayByComma, toNumber } from "./utils/parsing";
 import { validateBonusNumber, validateCost, validateLottoNumbers } from "./domains/validate";
+import { createLottos } from "./domains/createLottoNumbers";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { randomUniquesInRange } from "./utils/random";
+
 class App {
   async run() {
-    const tickets = await askUntilValid({
+    const purchasedAmount = await askUntilValid({
       question: INPUT_QUESTION.COST,
       parse: toNumber,
       makeAndValidate: validateCost
     });
+    const ticketAmount = devisionNumber(purchasedAmount, LOTTO_CONSTANTS.TICKET_PRICE);
+    const lottos = createLottos(ticketAmount, randomUniquesInRange);
+
+    MissionUtils.Console.print(`${ticketAmount}개를 구매했습니다.`);
+    lottos.forEach((lotto) => {
+      MissionUtils.Console.print(`[${lotto.numbers.join(", ")}]`);
+    });
+
     const winningNums = await askUntilValid({
       question: INPUT_QUESTION.WINNING_NUMS,
       parse: parseToArrayByComma,
