28bdf34d194a3d8d222a14bc15c065f8822e7697
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2025-11-03 19:26:46 +0900

    feat(domain): implement create lotto numbers based on tickets quantity
    로또 티켓 수만큼의 로또 숫자 세트를 생성합니다.

--- __tests__/CreatLottoDomain.test.js
+++ __tests__/CreatLottoDomain.test.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { creatOneLotto } from "../src/domains/createLottoNumbers";
+import { createLottos, creatOneLotto } from "../src/domains/createLottoNumbers";
 import { randomUniquesInRange } from "../src/utils/random";
 
 const mockRandoms = (numbers) => {
@@ -10,8 +10,20 @@ const mockRandoms = (numbers) => {
 };
 
 describe("로또 번호 생성 테스트", () => {
-  test("1~45 사이의 중복되지 않는 숫자 6개를 생성한다.", () => {
+  test("1~45 사이의 중복되지 않는 숫자 6개를 반환한다.", () => {
     mockRandoms([[1, 2, 3, 4, 44, 45]]);
     expect(creatOneLotto(randomUniquesInRange)).toStrictEqual([1, 2, 3, 4, 44, 45]);
   });
+  test("quantity만큼의 로또 세트를 반환한다..", () => {
+    mockRandoms([ // 6개 모킹
+      [1, 2, 3, 4, 44, 45],
+      [1, 2, 3, 4, 44, 45],
+      [1, 2, 3, 4, 44, 45],
+      [1, 2, 3, 4, 44, 45],
+      [1, 2, 3, 4, 44, 45],
+      [1, 2, 3, 4, 44, 45],
+    ])
+    // 5개만 생성 확인
+    expect(createLottos(5, randomUniquesInRange).length).toBe(5);
+  })
 })
--- src/domains/createLottoNumbers.js
+++ src/domains/createLottoNumbers.js
@@ -1,7 +1,12 @@
 import { LOTTO_CONSTANTS } from "../constants"
 
 export function creatOneLotto(drawUniqueNumbers) {
-  const {MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_TICKET} = LOTTO_CONSTANTS
-  return drawUniqueNumbers(MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_TICKET);  
+  const { MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_TICKET } = LOTTO_CONSTANTS
+  return drawUniqueNumbers( MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_TICKET );  
 };
 
+export function createLottos(quantity, drawUniqueNumbers) {
+  return Array.from({ length: quantity }, () => 
+    creatOneLotto(drawUniqueNumbers)
+  );
+};
